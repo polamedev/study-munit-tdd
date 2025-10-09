2191995bba4828dc3271ab3e722a0b9ec27e10be
polame <polame.dev@gmail.com>
2025-10-09 12:19:28 +0300

    Добавил mock для IO

---
+++ tests/mock/MockIO.c
@@ -0,0 +1,21 @@
+/**
+ * @file MockIO.c
+ * @author polame (polame.dev@gmail.com)
+ * @brief  Тестовый дублер мок 
+ * @date 2025-10-09
+ */
+
+#include <CppUTestExt/MockSupport_c.h>
+#include <MyLib/IO.h>
+
+void IO_Write(ioAddress addr, ioData value)
+{
+    mock_c()->actualCall("IO_Write")->withIntParameters("addr", addr)->withIntParameters("value", value);
+}
+
+ioData IO_Read(ioAddress addr)
+{
+    mock_c()->actualCall("IO_Read")->withIntParameters("addr", addr);
+
+    return mock_c()->returnValue().value.intValue;
+}
